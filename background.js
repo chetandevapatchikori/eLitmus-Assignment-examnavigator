3ff7bfa8afef2fc02e9f0ee2aec6b18e684f66bf
chetandevapatchikori <121754688+chetandevapatchikori@users.noreply.github.com>
2023-01-26 20:50:48 +0530

    Update background.js

--- background.js
+++ background.js
@@ -1,10 +1,12 @@
+
+// alert message shown while shifting the tabs
 chrome.tabs.onActivated.addListener(function (activeInfo) {
     chrome.tabs.get(activeInfo.tabId, function (tab) {
       alert("You switched to tab: "+ tab.url);
     });
   });
 
-
+// continues alert message shown when multiple tabs are opened.
   chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     // check if more than one tab is open
     if (tabId > 1) {
@@ -13,6 +15,8 @@ chrome.tabs.onActivated.addListener(function (activeInfo) {
     }
   });
 
+
+  //blocks the person from opening new tab
   chrome.tabs.onCreated.addListener(function(tab) {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
       chrome.tabs.remove(tabs[0].id);
@@ -20,33 +24,30 @@ chrome.tabs.onActivated.addListener(function (activeInfo) {
   });
 
 
-
+//Capturing user information in local storage 
   if (typeof localStorage === "undefined" || localStorage === null) {
     var LocalStorage = require('node-localstorage').LocalStorage;
-    localStorage = new LocalStorage('./scratch');
+    localStorage = new LocalStorage('./scratch');  // creating a new storage
   }
   
-  //Capturing user information
-  //Get user email
   var userEmail = localStorage.getItem('userEmail');
   if (!userEmail) {
-    //prompt user for email
     userEmail = prompt('Please enter your email address');
-    //store email in localStorage
-    localStorage.setItem('userEmail', userEmail);
+    localStorage.setItem('userEmail', userEmail);     //Get user email
   }
   
-  //Get user name
+  fetch('https://api.ipify.org/?format=json') // ip address for particular websites that we mention
+  .then(response => response.json())
+  .then(data => console.log(data.ip));  
+
+
   var userName = localStorage.getItem('userName');
   if (!userName) {
-    //prompt user for name
     userName = prompt('Please enter your name');
-    //store name in localStorage
-    localStorage.setItem('userName', userName);
+    localStorage.setItem('userName', userName);   //Get user name
   }
-  localStorage.clear();
+
+  localStorage.clear(); // for debuging purpose just clearing the local storage
 
 
-fetch('https://api.ipify.org/?format=json')
- .then(response => response.json())
- .then(data => console.log(data.ip));
+
